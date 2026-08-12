be45455ed55d98ac19b9893d168e6b37df40f380
chamika <chamika@softvilmedia.com>
2026-08-12 16:46:34 +0530

    use case completed to add by 1000 batches

--- Task1/src/climate/domain/usecases/load_data_usecase.py
+++ Task1/src/climate/domain/usecases/load_data_usecase.py
@@ -1,6 +1,8 @@
 # Author: Chamika Deshan
 # Created: 2026-08-12
 
+import csv
+import os
 from climate.domain.interfaces.iclimate_repository import IClimateRepository
 from core.util import ILogger
 
@@ -9,5 +11,34 @@ class LoadDataUseCase:
         self.climate_repository = climate_repository
         self.logger = logger
 
-    def execute(self, csv_file_path: str, batch_size: int = 1000) -> None:
-        self.logger.info(f"Climate data adding staryed...")
+    def execute(self, csv_file_path: str) -> None:
+        self.logger.info(f"data adding...")
+        
+        if not os.path.exists(csv_file_path):
+            self.logger.error(f"CSV not found: {csv_file_path}")
+            return
+
+        try:
+            with open(csv_file_path, mode='r', encoding='utf-8') as f:
+                reader = csv.DictReader(f)
+                batch = []
+                total_records = 0
+                
+                for row in reader:
+                    batch.append(row)
+                    if len(batch) >= 1000:
+                        self.climate_repository.write_records(batch)
+                        total_records += len(batch)
+                        batch = []
+                
+                # write remaining records
+                if batch:
+                    self.climate_repository.write_records(batch)
+                    total_records += len(batch)
+
+                self.logger.info(f"Ok adding records of :{total_records}")
+        except Exception as e:
+            self.logger.error(f"Error adding recrods: {str(e)}")
+        finally:
+            # finally we close always the con
+            self.climate_repository.close()
